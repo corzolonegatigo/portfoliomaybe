85cde185e6d47717d6b9527045ba4f542b7e2415
corzolonegatigo <zlee26369@gmail.com>
2024-06-25 23:24:22 +0800

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -12,12 +12,12 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritise open communication and collaboration in groupwork",
-    description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    title: "Interning part time at A*STAR",
+    description: "Currently",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh] ",
     imgClassName: "w-full h-full",
-    titleClassName: "justify-end",
-    img: "/b1.svg",
+    titleClassName: "justify-end drop-shadow-md text-white",
+    img: "/biiPortfolio.jpeg",
     spareImg: "",
   },
   {
@@ -46,24 +46,25 @@ export const featureprojects = [
   {
     id: 1,
     title: "Differentiating Between Medical Conditions with AI",
-    des: "Being developed in conjunction with A*STAR and SGH along 3 other teammates, to mitigate risk of misdiagnoses of medical issues.",
+    des: "Being developed in conjunction with A*STAR and SGH along 3 other teammates, to mitigate risk of misdiagnosis of medical issues.",
     img: "/biiPortfolio.jpeg",
     iconLists: ["python.svg","jupyter.svg","scikit-learn-seeklogo.svg"],
-    opt: "AI Model to save lives"
+    opt: "Private"
   },
   {
     id: 2,
-    title: "Automatic Piil Dispensor",
-    des: "Developed at a hackathon-styled event in a team, with the aim of ensuring adherence to medical prescriptions in elderly populations. ",
+    title: "Automatic Pill Dispensor",
+    des: "Developed at a hackathon-styled event in a team, with the aim of ensuring adherence to medical prescriptions in the elderly population. ",
     img: "/sutdPortfolio.jpeg",
     iconLists: ["arduino.svg","cpp_logo.svg"],
-    opt: "Uses Arduino Micro-controller"
+    opt: "https://docs.google.com/presentation/d/1acXAifk1toHtccAKwYVcd6Mb7JgGSChJUJtDTnPKBfY/present?usp=sharing",
+    link: "https://docs.google.com/presentation/d/1acXAifk1toHtccAKwYVcd6Mb7JgGSChJUJtDTnPKBfY/present?usp=sharing",
 
   },
   {
     id: 3,
     title: "Virtual Photo Booth using Computer Vision",
-    des: "Virtual Photo Booth for SST's 15th anniversary, developed as a coursework project",
+    des: "Virtual Photo Booth for SST's 15th anniversary, developed as a coursework project.",
     img: "/cv_coursework_portfolio.png",
     iconLists: ["/python.svg", "/opencv.svg"],
     link: "https://github.com/corzolonegatigo/Virtual-Photo-Booth",
@@ -73,7 +74,7 @@ export const featureprojects = [
   {
     id: 4,
     title: "Predicting Singapore Population Growth with AI",
-    des: "Using past recorded population data and scikit-learn machine learning library to predict future population size",
+    des: "Using past recorded population data and the scikit-learn machine learning library to predict future population size.",
     img: "/popPredict.png",
     iconLists: ["python.svg", "jupyter.svg", "/scikit-learn-seeklogo.svg"],
     link: url1,
@@ -84,24 +85,25 @@ export const projects = [
   {
     id: 1,
     title: "Differentiating Between Medical Conditions with AI",
-    des: "Being developed in conjunction with A*STAR and SGH along 3 other teammates, to mitigate risk of misdiagnoses of medical issues.",
+    des: "Being developed in conjunction with A*STAR and SGH along 3 other teammates, to mitigate risk of misdiagnosis of medical issues.",
     img: "/biiPortfolio.jpeg",
     iconLists: ["python.svg","jupyter.svg","scikit-learn-seeklogo.svg"],
-    opt: "AI Model to save lives"
+    opt: "Private",
   },
   {
     id: 2,
-    title: "Automatic Piil Dispensor",
-    des: "Developed at a hackathon-styled event in a team, with the aim of ensuring adherence to medical prescriptions in elderly populations. ",
+    title: "Automatic Pill Dispensor",
+    des: "Developed at a hackathon-styled event in a team, with the aim of ensuring adherence to medical prescriptions in the elderly population. ",
     img: "/sutdPortfolio.jpeg",
     iconLists: ["arduino.svg","cpp_logo.svg"],
-    opt: "Uses Arduino Micro-controller"
+    opt: "https://docs.google.com/presentation/d/1acXAifk1toHtccAKwYVcd6Mb7JgGSChJUJtDTnPKBfY/present?usp=sharing",
+    link: "https://docs.google.com/presentation/d/1acXAifk1toHtccAKwYVcd6Mb7JgGSChJUJtDTnPKBfY/present?usp=sharing",
 
   },
   {
     id: 3,
     title: "Virtual Photo Booth using Computer Vision",
-    des: "Virtual Photo Booth for SST's 15th anniversary, developed as a coursework project",
+    des: "Virtual Photo Booth for SST's 15th anniversary, developed as a coursework project.",
     img: "/cv_coursework_portfolio.png",
     iconLists: ["/python.svg", "/opencv.svg"],
     link: "https://github.com/corzolonegatigo/Virtual-Photo-Booth",
@@ -111,7 +113,7 @@ export const projects = [
   {
     id: 4,
     title: "Predicting Singapore Population Growth with AI",
-    des: "Using past recorded population data and scikit-learn machine learning library to predict future population size",
+    des: "Using past recorded population data and the scikit-learn machine learning library to predict future population size.",
     img: "/popPredict.png",
     iconLists: ["python.svg", "jupyter.svg", "/scikit-learn-seeklogo.svg"],
     link: url1,
@@ -120,19 +122,20 @@ export const projects = [
   {
     id: 5,
     title: "Textfighter in Python",
-    des: "Turn-Based Singleplayer Text-Based game developed in python. Working on making it in C#",
-    img: "/p4.svg",
+    des: "Turn-Based Singleplayer Text-Based game developed in python. Working on making it in C#.",
+    img: "/pythontextfighter.png",
     iconLists: ["python.svg"],
-    opt: "Textfighter in Python",
+    opt: "https://github.com/Felbadatcoding/Simple-Text-Fighter",
+    link: "https://github.com/Felbadatcoding/Simple-Text-Fighter"
   },
   {
     id: 6,
     title: "Portfolio Website",
-    des: "Made by following a tutorial as well as adding my personal touch to it, to customise it for my purposes. Hosted on Vercel.",
+    des: "Made by following a tutorial as well as adding my personal touch to it, to customise it for my purposes. Hosted on Vercel and uses UI components from the Aceternity UI Library.",
     img: "/portfolioPic_portfolio.png",
     iconLists: ["/next.svg", "/tail.svg", "/re.svg"],
-    link: "",
-    opt: "",
+    link: "https://github.com/corzolonegatigo/portfoliomaybe",
+    opt: "https://github.com/corzolonegatigo/portfoliomaybe",
   },
 ];
 
@@ -166,15 +169,15 @@ export const testimonials = [
   {
     quote: "/smu-sst camp.jpeg",
     name: "SMU Technopreneurship Camp",
-    title: "2-Day Hackathon Styled camp where my team desigend a solution for young technopreneurs lacking ways to platform themselves",
+    title: "2-Day Hackathon-styled camp where my team designed a solution for young technopreneurs lacking ways to platform themselves",
   },
   {
     quote: "/sieberrsecCert.png",
     name: "Sieberrsec CTF",
-    title: "Achieved 12th place in this DSTA-Sponsored cybersecurity competition",
+    title: "Achieved 12th place in this National DSTA-Sponsored cybersecurity competition organised by Hwa Chong Institute",
   },
   {
-    quote: "/b1.svg",
+    quote: "/aiocertPortfolio.png",
     name: "Australian Informatics Olympiad",
     title: "",
   },
